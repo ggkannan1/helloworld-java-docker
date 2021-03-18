c314f9cc253f8062f02d1d1745aaa5429ff8f493
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2021-03-18 06:32:47 +0000

    added dockerfile
     Committer: Kannan Panneerselvam <ggkannan1@gmail.com>

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, Welcome to Devops!");
+    System.out.println("We are exploring Jenkins");
+    System.out.println("We are exploring Build Triggers-Poll SCM");
+    System.out.println("We are exploring Build Triggers-Build Periodically");
+    System.out.println("We are exploring Build Triggers-Webhook");
+    System.out.println("We are testing Slave test jenkins using webhook");
+  }
+}
